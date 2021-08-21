ad309fd2151edf2c7da796e63cd103e582b55d17
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2021-08-21 16:23:17 -0300

    ⚙️ - Adding Dependences and Nodemon Start

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('Hello World 👋');
+const path = require('path');
+const cors = require('cors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.get('/', function(req,res){
+    res.json({message: 'Hello World'})
+})
+
+app.listen(port, function(){
+    console.log(`Servidor está rodando na porta ${port}`)
+})
